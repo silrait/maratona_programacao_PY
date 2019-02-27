c58e3652bf4001e92b8341704d561f2e6913226d
silrait <thiarllisba@hotmail.com>
2019-02-27 23:31:38 +0000

    Exercicio A  warmup 2009

--- 2009/warmup/A.py
+++ 2009/warmup/A.py
@@ -1,2 +1,29 @@
-for linha in sys.stdin:
+import sys
+
+
+def bubblesort(q, v):
+    jogador = ["marcelo", "carlos"]
+    turno = -1
+    for i in range(0, q):
+        for j in range(0, q - i - 1):
+            if(v[j] > v[j + 1]):
+                v[j], v[j + 1] = v[j + 1], v[j]
+                turno += 1
     
+    return jogador[turno % 2]
+    
+
+def main():
+    for linha in sys.stdin:
+        tokens = linha.strip().split(" ")
+        quantidade = int(tokens[0])
+        if(quantidade == 0):
+            break
+            
+        vetor = list(map(int, tokens[1:]))
+        
+        print(bubblesort(quantidade, vetor))
+        
+        
+if(__name__ == '__main__'):
+    main()
